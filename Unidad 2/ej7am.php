d4f17ecc3cc523f66f96db279d0255ac1de809e6
Ayman <contacto.ayman@gmail.com>
2023-10-04 19:20:53 +0200

    practica 7am terminada

--- Unidad 2/ej7am.php
+++ Unidad 2/ej7am.php
@@ -18,46 +18,38 @@
 		$nota_max = 10;
 		
 		$arrayMulti = array([],[]);
-		
-		$numero_maximo = array();
-		$numero_maximo = array_fill(0,$filas,$nota_min); // Relleno el array desde 0 hasta nº de filas con 0s
-		$numero_minimo= array();
-		$numero_minimo = array_fill(0,$filas,$nota_max); // Relleno el array desde 0 hasta nº de filas con 0s
-		$numero_promedio = array();
-		$numero_promedio = array_fill(0,$filas,0); // Relleno el array desde 0 hasta nº de filas con 0s
+		$notas_alumnos_promedio= array();
+		$notas_alumnos_promedio = array_fill(0,$filas,0); // Relleno el array desde 0 hasta nº de filas con 0s
 		
 		
 		// Por cada fila, recorro X columnas
 		for($f = 0; $f < $filas; $f++){
 			for($c = 0; $c < $columnas; $c++){
 					
-					// Genero un numero y lo reviso
-					$numero_rand = rand($nota_min,$nota_max);
-					if($numero_maximo[$f] < $numero_rand){
-						$numero_maximo[$f] = $numero_rand;
-					}
-					
-					if($numero_minimo[$f] > $numero_rand) {
-						$numero_minimo[$f] = $numero_rand;
-					}
-					
-				$numero_promedio[$f] += $numero_rand;
+				// Genero un numero y lo reviso
+				$numero_rand = rand($nota_min,$nota_max);
+				
+				$notas_alumnos_promedio[$f] += $numero_rand;
 					
 				$arrayMulti[$f][$c] = $numero_rand;
 				echo $arrayMulti[$f][$c]." ";
 			}
+			
+			$notas_alumnos_promedio[$f] /= $columnas;
+			
 			echo "</br>";	
 		}
 		
+		
+		
+		
+		
 		echo "</br>";
 		echo "</br>";
-		echo "Notas Maximas -> ";
-		print_r($numero_maximo);
-		
-		echo "</br>";
-		echo "</br>";
-		echo "Notas Minimas -> ";
-		print_r($numero_minimo);
+		for($i=0; $i<$filas; $i++){
+			echo "</br>";
+			printf("El alumno ".$i." tiene una media de todas las asignaturas de: ".$notas_alumnos_promedio[$i]);
+		}
 		
 		
 		
@@ -66,28 +58,90 @@
 		
 		
 		
-		$index_alumno_minimo;
-		$index_alumno_maximo;
 		// Por cada columna, recorro X filas
 		
 		echo "</br>";
 		echo "</br>";
 		
+		$asig_max;
+		$asig_min;
+		$nom_max;
+		$nom_min;
+		$n_max;
+		$n_min;
+		$notas_media = array();
+		
+		$notas_asignaturas_promedio = array();
+		$notas_asignaturas_promedio = array_fill(0,$filas,0); // Relleno el array desde 0 hasta nº de filas con 0s
+		
 		for($cc = 0; $cc < $columnas; $cc++){
 			for($ff = 0; $ff < $filas; $ff++){
-				echo $arrayMulti[$ff][$cc]."</br>";
+					
+						if($arrayMulti[$ff][$cc] > $nota_min){
+							$nota_min = $arrayMulti[$ff][$cc];
+							$n_max= $arrayMulti[$ff][$cc];
+							$nom_max= $ff;
+							$asig_max = $cc;
+						}
+						if($arrayMulti[$ff][$cc] < $nota_max){
+							$nota_max= $arrayMulti[$ff][$cc];
+							$n_min= $arrayMulti[$ff][$cc];
+							$nom_min= $ff;
+							$asig_min = $cc;
+						}
+						
+						$notas_asignaturas_promedio[$cc] += $arrayMulti[$ff][$cc];
 			}
+			
+			$notas_asignaturas_promedio[$cc] = $notas_asignaturas_promedio[$cc] / $filas;
 			echo " ";
 			echo "</br>";
 		}
 		
 		
-				
-				
-			
+		echo "La maxima nota fue de ".$nom_max." con un ".$n_max." en la asignatura de ".$asig_max;
+		echo "</br>";
+		echo "La minima nota fue de ".$nom_min." con un ".$n_min." en la asignatura de ".$asig_min;		
+		
+		echo "</br>";
+		for($i=0; $i<$columnas; $i++){
+			echo "</br>";
+			printf("En la asignatura ".$i." los alumnos tienen la media de ".$notas_asignaturas_promedio[$i]);
+		}
 			
 		
 			
+			
+		
+
+		
+		echo "</br>";
+		echo "</br>";
+		
+		$alumno = 3; // quiero saber las notas de este
+		$notas_bajo_alta = array(0,10);
+		$notas_materia = array();
+		
+		if($alumno < $filas){
+			for($ccc = 0; $ccc < $columnas; $ccc++){
+				
+				if($arrayMulti[$alumno][$ccc] > $notas_bajo_alta[0]){
+					$notas_bajo_alta[0] = $arrayMulti[$alumno][$ccc];
+					$notas_materia[0] = $ccc;
+				}
+				if($arrayMulti[$alumno][$ccc] < $notas_bajo_alta[1]){
+					$notas_bajo_alta[1] = $arrayMulti[$alumno][$ccc];
+					$notas_materia[1] = $ccc;
+				}
+			}
+			
+			echo "El alumno ".$alumno." ha sacado minimo un ".$notas_bajo_alta[0]." en ".$notas_materia[0];
+			echo "</br>";
+			echo "El alumno ".$alumno." ha sacado maximo un ".$notas_bajo_alta[1]." en ".$notas_materia[1];
+		}
+		
+		echo "</br>";	
+		
 		
 	?>
 	
