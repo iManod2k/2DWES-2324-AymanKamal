d9f396c70d5fe5471e6f9ce15657a759439ff9bc
Ayman <contacto.ayman@gmail.com>
2023-10-09 10:19:14 +0200

    ej1 de Calculadora hecho

---
+++ Unidad 2/forms/calculadora/calculadora.php
@@ -0,0 +1,17 @@
+<?php
+	echo "<h1>Calculadora</h1>";
+	echo "metodo usado -> ".$_SERVER["REQUEST_METHOD"];
+	
+	$num1 = $_POST['op1'];
+	$num2= $_POST['op2'];
+	$resul;
+	
+	$tipo_operacion = $_POST['tipo_op'];
+	switch($tipo_operacion){
+		case "s" : printf("<h4>El valor de %u + %u = %u</h4>", $num1, $num2, ($num1+$num2)); break;
+		case "r" : printf("<h4>El valor de %u - %u = %u</h4>", $num1, $num2, ($num1-$num2)); break;
+		case "m" : printf("<h4>El valor de %u * %u = %u</h4>", $num1, $num2, ($num1*$num2)); break;
+		case "d" : printf("<h4>El valor de %u / %u = %u</h4>", $num1, $num2, ($num1/$num2)); break;
+	}
+	
+?>
